af8b4d032a76a3e15a60c7da18629134cec815bd
lidia <lramirez@naviomar.com.mx>
2026-06-12 16:09:59 +0000

    corrección

--- pages/system/admin-department/index.vue
+++ pages/system/admin-department/index.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <h1 class="text-xl font-bold underline mb-2">Admin department users</h1>
     <DepartmentAdminForm 
-        :id="String(id_department)"
+        :id="String(idDepartment())"
     />
   </v-container>
 </template>
@@ -15,43 +15,51 @@ definePageMeta({
   title: `Admin department users`,
   layout: 'default',
 })
-console.log("--->",user.value.roles[0])
-if(user.value.roles[0]){
-  const rol = user.value.roles[0].id;
-  let id_department = 0;
-  
-  switch (rol) {
-    case 1:
-        id_department = 9;
-      break;
-    case 4:
-        id_department = 1;
-      break;
-    case 5:
-        id_department = 2;
-      break;
-    case 6:
-        id_department = 3;
-      break;
-    case 7:
-        id_department = 4;
-      break;
-    case 8:
-        id_department = 6;
-      break;
-    case 9:
-        id_department = 8;
-      break;
-    case 11:
-      id_department = 5;
-    break;
-   
-    default:
-      break;
-  }
-}else{
-  snackbar.add({ type: 'error', text: 'Usted no tiene permisos para realizar esta acción.' })
+
+function idDepartment(){
+    if(!user.value.roles[0]) return;
+
+    if(user.value.roles[0]){
+      const rol = user.value.roles[0].id;
+      let id_department = 0;
+      
+      switch (rol) {
+        case 1:
+            id_department = 9;
+          break;
+        case 4:
+            id_department = 1;
+          break;
+        case 5:
+            id_department = 2;
+          break;
+        case 6:
+            id_department = 3;
+          break;
+        case 7:
+            id_department = 4;
+          break;
+        case 8:
+            id_department = 6;
+          break;
+        case 9:
+            id_department = 8;
+          break;
+        case 11:
+          id_department = 5;
+        break;
+      
+        default:
+          break;
+      }
+      
+      return id_department
+    }else{
+      snackbar.add({ type: 'error', text: 'Usted no tiene permisos para realizar esta acción.' })
+    }
 }
+ 
+
   
 
 </script>
